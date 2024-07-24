b5efe1e9aa14858f71dd166639eae4aa2a1e2cfd
colinasdelsur <colinasdelsur@hotmail.com>
2024-07-24 13:15:49 -0600

    24 julio 2024

---
+++ app/Livewire/Quick/Counter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Quick;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public function render()
+    {
+        return view('livewire.quick.counter');
+    }
+}

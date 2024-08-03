054dde764aee365f7cbd1794b913d854c528a4c2
colinasdelsur <colinasdelsur@hotmail.com>
2024-08-02 18:30:39 -0600

    titlexxx

--- app/Livewire/Compon/CreatePost.php
+++ app/Livewire/Compon/CreatePost.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class CreatePost extends Component
 {
-    public $title = 'Post Titlexx';
+    public $title = 'Post Titlexxx';
 
     public function render(){
         return view('livewire.compon.create-post')->with([

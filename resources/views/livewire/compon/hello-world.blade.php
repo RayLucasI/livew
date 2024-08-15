17e44e224a464e19b4f1a0679b9160bdf45b0ca0
colinasdelsur <colinasdelsur@hotmail.com>
2024-08-15 14:09:03 -0600

    Button on wire.

--- resources/views/livewire/compon/hello-world.blade.php
+++ resources/views/livewire/compon/hello-world.blade.php
@@ -1,17 +1,9 @@
 <div>
     <div>The curren time es: {{ time() }}</div><br>
 
-    <button 
-        wire:click="$refresh" class="bg-blue-500 
-                    hover:bg-blue-700 text-white font-bold 
-                    py-2 px-4 rounded-full">
-        Button
-    </button>    
-
     <button wire:click="$refresh"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         Button
     </button>
-
 
 </div>
